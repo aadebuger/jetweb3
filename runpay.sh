5997791ec6c3b1eec824f75a88cf856e2c8d1156
aadebuger <zhvxxh@gmail.com>
2015-06-27 13:50:30 +0800

    CELERY_BROKER_UR

--- runpay.sh
+++ runpay.sh
@@ -1,1 +1,1 @@
-docker run -d -e PINGPP_APP_ID=app_5arn50CePG844W58   -e PINGPP_APP_KEY=sk_live_nMnwcL8fzWdhdiVltgi29YmY  --name pingpp0 -p 8888:8888 aadebuger/pingpp
+docker run -d -e  -e "CELERY_BROKER_URL=amqp://guest@1257.net" PINGPP_APP_ID=app_5arn50CePG844W58   -e PINGPP_APP_KEY=sk_live_nMnwcL8fzWdhdiVltgi29YmY  --name pingpp0 -p 8888:8888 aadebuger/pingpp
