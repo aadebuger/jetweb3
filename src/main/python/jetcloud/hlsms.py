5be853227662c8f615929789fa0d73b35ed1e4ca
aadebuger <30758919@qq.com>
2015-11-15 15:35:40 +0800

    changed

--- src/main/python/jetcloud/hlsms.py
+++ src/main/python/jetcloud/hlsms.py
@@ -6,10 +6,13 @@ Created on Aug 5, 2015
 '''
 import requests
 import json
+import urllib
 from  urllib  import unquote
 def hlquicksend(phone,smscode):
     
-        payload = {'linkid':'','subcode':'','username':'zrxt','password':'password777','epid':'109430','phone':phone,'message':"您的验证码是：%s。请不要把验证码泄露给其他人。"%(smscode)}
+        message = "您的验证码是：%s。请不要把验证码泄露给其他人。"%(smscode)
+
+        payload = {'linkid':'','subcode':'','username':'zrxt','password':'password777','epid':'109430','phone':phone,'message':urllib.quote_plus(message.decode("utf-8").encode("gb2312"))}
 
         ret =requests.get('http://114.255.71.158:8061',params=payload)
         print 'ret=',ret.text
