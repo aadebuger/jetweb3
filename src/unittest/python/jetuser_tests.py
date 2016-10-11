ee663bc37fa9c6195f009656a0f5d78e1155e5b0
zhuanghua <30758919@qq.com>
2016-10-11 15:42:02 +0800

    log

---
+++ src/unittest/python/jetuser_tests.py
@@ -0,0 +1,37 @@
+'''
+Created on Oct 11, 2016
+
+@author: aadebuger
+'''
+import unittest
+import time
+from jetcloud import jetuser
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+            user1 =jetuser.User.querybyauthData("weixin","0395BA18A5CD6255E5BA185E7BEBA242")
+            print("authData user1",user1)
+    def testNewUser(self):
+                        user = jetuser.User(username="LeanCloud")
+                        user.authData={
+  "authData": {
+    "weixin": {
+      "openid": "0395BA18A5CD6255E5BA185E7BEBA242",
+      "access_token": "12345678-SaMpLeTuo3m2avZxh5cjJmIrAfx4ZYyamdofM7IjU",
+      "expires_in": 1382686496
+    }
+  }
+}
+                        user.password="123456"
+                        user.createdAt=time.strftime('%Y-%m-%dT%H:%M:%S')
+                        user.updatedAt=time.strftime('%Y-%m-%dT%H:%M:%S')
+            
+                                
+                        user.save()
+                        
+                        
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
