c4b725d6c9aa05fd7eea70a94047b11af28d4ffa
zhuanghua <30758919@qq.com>
2016-11-17 11:45:27 +0800

    src/main/python/jetcloud/restobject.py

--- src/main/python/jetcloud/restobject.py
+++ src/main/python/jetcloud/restobject.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """
 Created on Nov 14, 2015
 
@@ -74,6 +75,19 @@ def rest2mongo(restdict):
                 for item in value:
 #                    print 'item=',item
                     fixupvalue(item)
+def formatrest2mongo(restdict):
+        for key in  restdict:
+            print 'key=',key
+            if key=='createdAt' or key =='fanlidate' or key =='updatedAt':
+                value=restdict[key]
+                restdict[key]=value['iso']
+            else:
+                value=restdict[key]
+
+                print(isinstance(value, dict))
+                if isinstance(value, dict):
+                    print("dict")
+                    formatrest2mongo(value)
 if __name__ == "__main__":
     print "restobject"
     jsonstr1="""{"status":1}"""
@@ -82,8 +96,10 @@ if __name__ == "__main__":
     jsonstr="""{"status": {"$gte": 0}, "createdAt": {"$gte": {"__type": "Date", "iso": "2015-11-01T16:00:00.000Z"},"$lte": {"__type": "Date", "iso": "2015-11-14T16:00:00.000Z"}}}"""
     jsonstr="""{"$and":[{"objectId":"57e0dd1da22b9d0061248912"}]}"""
     jsonstr="""{"mobilePhoneNumber":{"$exists":true},"updatedAt":{"$gte":{"__type":"Date","iso":"2016-11-14T07:17:55.000Z"}}}"""
-    dict = json.loads(jsonstr)
-    print "dict=",dict
+    jsonstr="""{"type":"INFORMATION","status":"enabled","content":{"title":"年报炒作浪将起","url":"http://ac-3G47drEA.clouddn.com/8fc3d82fc146df31f379.html","updatedAt":{"__type":"Date","iso":"2016-11-15T13:21:59.287Z"},"summary":"随着三季报落幕，A股市场已经进入年报炒作阶段，。数据显示，目前共计有超1130家公司发布2016年年报业绩预告，其中113家公司预告，其年报净利增长下限至少翻番，其中42只个股净利润至少翻三番。","thumbnail":"http://ac-3G47drEA.clouddn.com/6294566f969d67d166d7.png"}}"""
+    
+    dict1 = json.loads(jsonstr)
+    print "dict=",dict1
 #    fixup(dict,"aa","bb")
-    rest2mongo(dict)
-    print 'dict=',dict
+    formatrest2mongo(dict1)
+    print 'dict=',dict1
