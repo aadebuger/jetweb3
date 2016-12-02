3699f77b82fc9ed95d904b697cd6d45f46e1592f
zhuanghua <30758919@qq.com>
2016-12-02 19:14:26 +0800

    pushdict

--- src/main/python/jetcloud/MongoResource.py
+++ src/main/python/jetcloud/MongoResource.py
@@ -608,9 +608,9 @@ class MResource(Resource):
             else:
                 setdict['updatedAt']=getIso8601()
                 ret = db[self.documentname].update({'_id': ObjectId(todo_id)},{"$push":pushdict,"$set":setdict }) 
-                print 'ret=',ret
-                retdict = {"objectId":todo_id,"updatedAt":updatedAt}
-                self.after_put(todo_id,request.json,"put");
+            print 'ret=',ret
+            retdict = {"objectId":todo_id,"updatedAt":updatedAt}
+            self.after_put(todo_id,request.json,"put");
 
                             
 #            client.close()
