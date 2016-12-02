7b9478e11f9142a9f79162e7de9c0c3040cbb308
zhuanghua <30758919@qq.com>
2016-12-02 16:45:14 +0800

    format put

--- src/main/python/jetcloud/restobject.py
+++ src/main/python/jetcloud/restobject.py
@@ -103,6 +103,40 @@ def formatpost2mongo(restdict):
                             newvalue=value['objects']
                             
                             restdict[key]=newvalue
+def formatput2mongo(restdict):
+        setdict={};
+        pushdict={}
+        for key in  restdict:
+                print 'post key=',key
+                pushtype=False
+                value = restdict[key]
+                if isinstance(value, dict):
+                    print("dict")
+                    if value.has_key("__op"):
+                        opvalue = value["__op"]
+                        if opvalue=='Add':
+                            
+                            pushtype=True
+                            pushdict[key]={"$each":value["objects"] }
+                if pushtype is False:
+                    setdict[key]=restdict[key]
+        return (setdict,pushdict)    
+def isOp(self,mydict):
+            print 'mydict',mydict
+            myitemlist = mydict.items()
+            print 'myitemlist',myitemlist
+            if len(myitemlist)==1:
+                (key,value)= myitemlist[0]
+                print 'key',key
+                if isinstance(value, dict):
+                    print 'isinstance'
+                    if value.has_key("__op") and  value.has_key("objects"):
+                            
+                            op = value["__op"]
+                            print 'op=',op
+                            if op =='Add':
+                                return ("$push",{key: {"$each":value["objects"] } })
+            return None
 
                                     
 if __name__ == "__main__":
@@ -115,9 +149,11 @@ if __name__ == "__main__":
     jsonstr="""{"mobilePhoneNumber":{"$exists":true},"updatedAt":{"$gte":{"__type":"Date","iso":"2016-11-14T07:17:55.000Z"}}}"""
     jsonstr="""{"type":"INFORMATION","status":"enabled","content":{"title":"年报炒作浪将起","url":"http://ac-3G47drEA.clouddn.com/8fc3d82fc146df31f379.html","updatedAt":{"__type":"Date","iso":"2016-11-15T13:21:59.287Z"},"summary":"随着三季报落幕，A股市场已经进入年报炒作阶段，。数据显示，目前共计有超1130家公司发布2016年年报业绩预告，其中113家公司预告，其年报净利增长下限至少翻番，其中42只个股净利润至少翻三番。","thumbnail":"http://ac-3G47drEA.clouddn.com/6294566f969d67d166d7.png"}}"""
     jsonstr="""{"reminder": {"objects": ["1", "2"], "__op": "Add"}}"""
-    
+    jsonstr="""{"test":"1","reminder": {"objects": ["1", "2"], "__op": "Add"}}"""
+    jsonstr="""{"reminder": {"objects": ["1", "2"], "__op": "Add"}}"""
     dict1 = json.loads(jsonstr)
     print "dict=",dict1
 #    fixup(dict,"aa","bb")
-    formatpost2mongo(dict1)
-    print 'dict=',dict1
+#    formatpost2mongo(dict1)
+    ret=formatput2mongo(dict1)   
+    print(ret)
